6df873aff386205d08298cd76989c9f0d5ed12a9
Motasim Foad <motasimfoad@gmail.com>
2020-11-24 17:49:11 +0600

    changed images

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
     var year = new Date().getFullYear();
-    var yetiIndex = Math.floor(Math.random() * 10);
+    var yetiIndex = Math.floor(Math.random() * 9);
     var cloudsArray = $("img");
     console.log(cloudsArray[yetiIndex]);
     $("#year").html(year)
     
     $("img").click(function() {
         if (yetiIndex == $(this).attr("value")) {
-           $(this).attr("src", "./Assets/images/yeti.jpg");
+           $(this).attr("src", "./Assets/images/yeti.png");
         } else {
             $(this).attr("src", "./Assets/images/penguin.png");
         }
