c62649cc73d8e11938ad9859cea196d4c995ded7
Motasim Foad <motasimfoad@gmail.com>
2020-11-24 20:05:10 +0600

    added game over popup

--- JS/app.js
+++ JS/app.js
@@ -2,16 +2,21 @@ $(document).ready(function(){
     var year = new Date().getFullYear();
     var yetiIndex = Math.floor(Math.random() * 9);
     var cloudsArray = $("img");
-    console.log(cloudsArray[yetiIndex]);
+    var score = 0;
     $("#year").html(year)
-    
+   
     $("img").click(function() {
         if (yetiIndex == $(this).attr("value")) {
-           $(this).attr("src", "./Assets/images/yeti.png");
+            $(this).attr("src", "./Assets/images/yeti.png");
+            $(".popup-overlay, .popup-content").addClass("active");
         } else {
             $(this).attr("src", "./Assets/images/penguin.png");
+            score++;
         }
-       
+        console.log(score);
       });
-    
+      $("#close").click(function() {
+          console.log("lala");
+        location.reload(); 
+      });
   }); 
