3e774f7a314d5040ebb166b0e0b7f771abdb1b3b
Motasim Foad <motasimfoad@gmail.com>
2020-11-24 16:48:27 +0600

    basic game logic added

--- JS/app.js
+++ JS/app.js
@@ -1,12 +1,17 @@
 $(document).ready(function(){
     var year = new Date().getFullYear();
     var yetiIndex = Math.floor(Math.random() * 10);
+    var cloudsArray = $("img");
+    console.log(cloudsArray[yetiIndex]);
     $("#year").html(year)
-
-    $("clouds").hover(function(){
-        $(this).css("backgroundColor", "red")
-    },
-    function(){
-        $(this).css("color", "black")
-    });
+    
+    $("img").click(function() {
+        if (yetiIndex == $(this).attr("value")) {
+           $(this).attr("src", "./Assets/images/yeti.jpg");
+        } else {
+            $(this).attr("src", "./Assets/images/penguin.png");
+        }
+       
+      });
+    
   }); 
