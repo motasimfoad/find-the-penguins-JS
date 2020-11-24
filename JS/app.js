0ebadb6e664c340149705281ae2d8ca9156d3d2c
Motasim Foad <motasimfoad@gmail.com>
2020-11-24 21:18:29 +0600

    game version beta 1

--- JS/app.js
+++ JS/app.js
@@ -4,17 +4,32 @@ $(document).ready(function(){
     var cloudsArray = $("img");
     var score = 0;
     $("#year").html(year)
-   
+   console.log(yetiIndex);
     $("img").click(function() {
-        if (yetiIndex == $(this).attr("value")) {
-            $(this).attr("src", "./Assets/images/yeti.png");
+        if (score == 8) {
+            $(".popup-image").attr("src", "./Assets/images/penguin.png");
+            $("#winLoss").text("You Won!!");
+            $("#goodNews").text("You found all the penguins");
+            $("#winLoss").css( "color", "green" );
             $(".popup-overlay, .popup-content").addClass("active");
         } else {
-            $(this).attr("src", "./Assets/images/penguin.png");
-            score++;
+            if (yetiIndex == $(this).attr("value")) {
+                $(this).attr("src", "./Assets/images/yeti.png");
+                $(".popup-overlay, .popup-content").addClass("active");
+            } else {
+                $(this).attr("src", "./Assets/images/penguin.png");
+                if ($(this).attr("value") == "clicked") {
+                    alert("This door is already opened!!");
+                }else{
+                    score++;
+                    $(this).attr("value", "clicked");
+                }
+            }
+            console.log(score);  
         }
-        console.log(score);
+        
       });
+
       $("#restart").click(function() {
         location.reload(); 
       });
