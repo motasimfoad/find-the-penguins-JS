2937bbcff2ec3a49e15c5c4f002851a3a42718e8
Motasim Foad <motasimfoad@gmail.com>
2020-11-24 21:56:11 +0600

    added sound effects

--- JS/app.js
+++ JS/app.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
     var year = new Date().getFullYear();
     var yetiIndex = Math.floor(Math.random() * 9);
-    var cloudsArray = $("img");
     var score = 0;
-    $("#year").html(year)
-   console.log(yetiIndex);
+    var tickAudio = new Audio('./Assets/audio/tick.mp3');
+    var bombAudio = new Audio('./Assets/audio/bomb.mp3');
+    var tadaAudio = new Audio('./Assets/audio/tada.mp3');
+    
+    $("#year").html(year);
     $("img").click(function() {
         if (score == 8) {
+            tadaAudio.play();
             $(".popup-image").attr("src", "./Assets/images/penguin.png");
             $("#winLoss").text("You Won!!");
             $("#goodNews").text("You found all the penguins");
@@ -14,10 +17,12 @@ $(document).ready(function(){
             $(".popup-overlay, .popup-content").addClass("active");
         } else {
             if (yetiIndex == $(this).attr("value")) {
+                bombAudio.play();
                 $(this).attr("src", "./Assets/images/yeti.png");
                 $(".popup-overlay, .popup-content").addClass("active");
             } else {
                 $(this).attr("src", "./Assets/images/penguin.png");
+                tickAudio.play();
                 if ($(this).attr("value") == "clicked") {
                     alert("This door is already opened!!");
                 }else{
@@ -25,9 +30,7 @@ $(document).ready(function(){
                     $(this).attr("value", "clicked");
                 }
             }
-            console.log(score);  
         }
-        
       });
 
       $("#restart").click(function() {
